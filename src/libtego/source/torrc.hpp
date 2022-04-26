6704aea28eb00e29f36c34ed97daf950b8622420
Marco Simonelli <msimonelli@protonmail.com>
2022-04-26 22:58:00 +1000

    Pluggables: Implemented pluggable transport support, todo UI

---
+++ src/libtego/source/torrc.hpp
@@ -0,0 +1,4 @@
+#pragma once
+// The contents of this string is populated in ricochet-build, in order to set
+// the path to the pluggable transport binaries.
+const std::string defaultTorrcPluggableConfig = "";
