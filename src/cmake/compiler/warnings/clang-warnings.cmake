ea7cd9f5511ed9d46769d7a1b50f638fae5ccc19
Wolfgang Frisch <wfrisch@riseup.net>
2022-05-01 02:51:37 +0200

    -Wno-shadow

--- src/cmake/compiler/warnings/clang-warnings.cmake
+++ src/cmake/compiler/warnings/clang-warnings.cmake
@@ -3,7 +3,7 @@
 set(CXX_WARNINGS
     -Wall
     -Wextra # reasonable and standard
-    -Wshadow # warn the user if a variable declaration shadows one from a parent context
+    -Wno-shadow # warn the user if a variable declaration shadows one from a parent context
     -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps catch
                        # hard to track down memory errors
     -Wold-style-cast # warn for c-style casts
